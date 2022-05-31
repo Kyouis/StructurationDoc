d61d3ae99900f4461592a96bd9a55c23a2d4b579
Kyouis <45875365+Kyouis@users.noreply.github.com>
2022-05-31 18:12:40 +0200

    recommandation

--- Structuration/src/main/java/Connection.java
+++ Structuration/src/main/java/Connection.java
@@ -10,22 +10,53 @@ import org.json.simple.parser.ParseException;
 
 import javax.print.Doc;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Connection {
-
+    static MongoCollection<Document> tags;
     static MongoCollection<Document> tracks;
     static MongoCollection<Document> artists;
+    static MongoCollection<Document> recom;
     public static void connect() {
         String uri = "mongodb://localhost:27017";
         MongoClient mongoClient = MongoClients.create(uri);
         MongoDatabase database = mongoClient.getDatabase("SD2022_projet");
         artists  = database.getCollection("artists", Document.class);
         tracks = database.getCollection("tracks", Document.class);
+        tags  = database.getCollection("tags", Document.class);
+        recom = database.getCollection("Recommandation", Document.class);
     }
 
     public static String getAPiKey() {
         return "f0f8ed2855eaf7851a62054c891d91a1";
+    }
+
+    public static void recom() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserez votre nom");
+        String nom = sc.nextLine();
+        String typeReco;
+        String note;
+        System.out.println("Que voulez-vous recommander ?\n" +
+                "1 - Album\n" +
+                "2 - Artiste\n" +
+                "3 - Musique\n");
+        String choix = sc.nextLine();
+        switch (choix) {
+            case "1" -> typeReco="Album";
+            case "2" -> typeReco = "Artiste";
+            case "3" -> typeReco = "Musique";
+            default -> typeReco = "";
+        }
+        System.out.println("Quelle note sur 5 donnez-vous ?");
+        choix = sc.nextLine();
+        if (Integer.parseInt(choix) > 5 || Integer.parseInt(choix)<0) {
+            System.out.println("Note non comprise entre 0 et 5");
+        } else {
+            recom.insertOne(new Document("utilisateur", nom).append("Oeuvre ou artiste noté", typeReco).append("note", choix));
+        }
+
     }
 
     public static void getTopArtists() throws ParseException {
@@ -41,8 +72,17 @@ public class Connection {
         }
     }
 
-    public static String getTopTags() {
-        return HTTPTools.sendGet("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key="+Connection.getAPiKey()+"&format=json&limit=10");
+    public static void getTopTags() {
+        String res = HTTPTools.sendGet("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key="+Connection.getAPiKey()+"&format=json&limit=10");
+                Document parseRes = Document.parse(res);
+        Document res2 = (Document) parseRes.get("tags");
+        ArrayList<Document> res3 = (ArrayList<Document>) res2.get("tag");
+        for (Document o : res3) {
+            String name = (String) o.get("name");
+            String reach= (String) o.get("reach");
+            String taggings = (String) o.get("taggings");
+            tags.insertOne(new Document("name", name).append("reach", reach).append("taggings", taggings));
+        }
     }
 
     public static void getTopTracks() {
--- Structuration/src/main/java/Main.java
+++ Structuration/src/main/java/Main.java
@@ -13,13 +13,15 @@ public class Main {
         System.out.println("Bienvenue dans l'application, que voulez-vous faire : \n" +
                 "1 - Top 10 des artistes\n" +
                 "2 - Top 10 des Genres de musique\n" +
-                "3 - Top 10 des musiques\n");
+                "3 - Top 10 des musiques\n" +
+                "4 - Noter");
 
         String choix = sc.nextLine();
         switch (choix) {
             case "1" -> Connection.getTopArtists();//System.out.println(Connection.getTopArtists());
-            case "2" -> System.out.println(Connection.getTopTags());
+            case "2" -> Connection.getTopTags();
             case "3" -> Connection.getTopTracks();
+            case "4" -> Connection.recom();
             default -> System.out.println("Entrez un choix valide");
         }
 
