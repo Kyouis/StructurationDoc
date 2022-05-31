807ce8160e5a8461f95b6e767c522e57fb62b6ac
Kyouis <45875365+Kyouis@users.noreply.github.com>
2022-05-31 16:48:03 +0200

    topArtists

--- Structuration/src/main/java/Connection.java
+++ Structuration/src/main/java/Connection.java
@@ -1,26 +1,42 @@
 package main.java;
 
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+
 
 public class Connection {
 
-
-
+    static MongoCollection<Document> artists;
     public static void connect() {
         String uri = "mongodb://localhost:27017";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("SD2022_projet");
-        }
+        MongoClient mongoClient = MongoClients.create(uri);
+        MongoDatabase database = mongoClient.getDatabase("SD2022_projet");
+        artists  = database.getCollection("artists", Document.class);
+
     }
 
     public static String getAPiKey() {
         return "f0f8ed2855eaf7851a62054c891d91a1";
     }
 
-    public static String getTopArtists() {
-        return HTTPTools.sendGet("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key="+Connection.getAPiKey()+"&format=json&limit=10");
+    public static void getTopArtists() throws ParseException {
+        String res = HTTPTools.sendGet("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key="+Connection.getAPiKey()+"&format=json&limit=10");
+        Document parseRes = Document.parse(res);
+        Document res2 = (Document) parseRes.get("artists");
+        ArrayList<Document> res3 = (ArrayList<Document>) res2.get("artist");
+        for (Document o : res3) {
+            String name = (String) o.get("name");
+            String listeners = (String) o.get("listeners");
+            String playcount = (String) o.get("playcount");
+            artists.insertOne(new Document("name", name).append("listeners", listeners).append("playcount", playcount));
+        }
     }
 
     public static String getTopTags() {
--- Structuration/src/main/java/Main.java
+++ Structuration/src/main/java/Main.java
@@ -1,12 +1,15 @@
 package main.java;
 
+import org.json.simple.parser.ParseException;
+
 import java.util.Scanner;
 
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Connection.connect();
         System.out.println("Bienvenue dans l'application, que voulez-vous faire : \n" +
                 "1 - Top 10 des artistes\n" +
                 "2 - Top 10 des Genres de musique\n" +
@@ -14,10 +17,12 @@ public class Main {
 
         String choix = sc.nextLine();
         switch (choix) {
-            case "1" -> System.out.println(Connection.getTopArtists());
+            case "1" -> Connection.getTopArtists();//System.out.println(Connection.getTopArtists());
             case "2" -> System.out.println(Connection.getTopTags());
             case "3" -> System.out.println(Connection.getTopTracks());
             default -> System.out.println("Entrez un choix valide");
         }
+
+
     }
 }
